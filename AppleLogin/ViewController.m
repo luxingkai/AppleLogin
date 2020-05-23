74d71f08857f6e4d5c7c55033c85fe5cf7514b04
tigerfly <tigerfly@tigerflydeiMac.local>
2020-05-23 09:51:32 +0800

    log

---
+++ AppleLogin/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  AppleLogin
+//
+//  Created by tigerfly on 2020/5/23.
+//  Copyright © 2020 tigerfly. All rights reserved.
+//
+
+
+/*
+ 处理苹果登录文档
+ https://developer.apple.com/documentation/authenticationservices/implementing_user_authentication_with_sign_in_with_apple?language=objc
+ */
+#import "ViewController.h"
+#import <AuthenticationServices/AuthenticationServices.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+}
+
+
+@end
